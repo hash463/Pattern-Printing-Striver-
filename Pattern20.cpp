0088e92190fefcbb9f70c96889497ca051cb7ad5
Harish Jayaprakash <jharish2003@gmail.com>
2023-05-04 13:59:54 +0530

    Create Pattern20.cpp

---
+++ Pattern20.cpp
@@ -0,0 +1,45 @@
+/*
+Given pattern is:
+
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+
+*/
+
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j>0; j--){
+                cout<<"*";
+            }
+            for(int j=((n*2)-(i+1)*2); j>0; j--){
+                cout<<" ";
+            }
+            for(int j=i+1; j>0; j--){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        for(int i=1; i<n; i++){
+            for(int j=n-i; j>0; j--){
+                cout<<"*";
+            }
+            for(int j=i*2; j>0; j--){
+                cout<<" ";
+            }
+            for(int j=n-i; j>0; j--){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+};
